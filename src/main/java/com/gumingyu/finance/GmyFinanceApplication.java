af87bbac4e14d39a60fdec150d6cda60a0d90c5d
daming_ocean <daming_ocean@hotmail.com>
2018-09-26 15:15:29 +0800

    项目创建

---
+++ src/main/java/com/gumingyu/finance/GmyFinanceApplication.java
@@ -0,0 +1,12 @@
+package com.gumingyu.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmyFinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmyFinanceApplication.class, args);
+    }
+}
