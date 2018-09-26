9572e44886b4d3656725db3db04879f4b034e921
daming_ocean <daming_ocean@hotmail.com>
2018-09-26 20:59:37 +0800

    增加checkstyle和sonar检查

--- src/main/java/com/gumingyu/finance/GmyFinanceApplication.java
+++ src/main/java/com/gumingyu/finance/GmyFinanceApplication.java
@@ -1,11 +1,19 @@
 package com.gumingyu.finance;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 程序入口
+ */
 @SpringBootApplication
+@Slf4j
 public class GmyFinanceApplication {
 
+    /**
+     * @param args main函数参数
+     */
     public static void main(String[] args) {
         SpringApplication.run(GmyFinanceApplication.class, args);
     }
